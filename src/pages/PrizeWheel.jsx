cc80a88c401418fec1b90029b4bf8498a7d21c8a
Giacomo Llinares <giacomo.llinares@gmail.com>
2022-03-31 08:30:24 +0200

    Added basic spinning wheel

--- src/pages/PrizeWheel.jsx
+++ src/pages/PrizeWheel.jsx
@@ -2,16 +2,33 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Pot from '../components/Pot'
 import HomeButton from '../components/HomeButton'
+import Wheel from '../assets/wheel.svg'
+import Pin from '../assets/pin.svg'
 
 const PrizeWheel = ({ coins, setCoins }) => {
   PrizeWheel.propTypes = {
     coins: PropTypes.number,
     setCoins: PropTypes.func
   }
+  function handleClick () {
+    if (coins > 0) {
+      setCoins(coins - 1)
+      const wheel = document.querySelector('.wheel')
+      wheel.classList.add('spinning')
+      setTimeout(() => {
+        wheel.classList.remove('spinning')
+      }, 4000)
+    }
+  }
   return (
     <div className='prize-wheel'>
-      <h1>Prizewheel - Work in progress</h1>
+      <h1>La Routourne</h1>
       {<Pot coins={coins}/>}
+      <div className="wheel-container">
+        <img className='pin' src={Pin} alt='pin' />
+        <img className='wheel' src={Wheel} alt='wheel'/>
+      </div>
+      <button className='btn-wheel' onClick={handleClick}>Tourner !</button>
       <HomeButton />
     </div>
   )
