1f9831fee970ed1fee69adcfdf970c0961cd75d6
Giacomo Llinares <giacomo.llinares@gmail.com>
2022-04-10 14:44:31 +0200

    minor fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -42,36 +42,32 @@ const Home = ({ saveGame, setSaveGame, progress, setProgress, setCoins }) => {
   }
 
   function checkCodeIntegrity (saveGame) {
-    console.log(saveGame)
     if (saveGame.currentQuestion !== saveGame.answers.length) return false
     let correctAnswers = 0
     saveGame.answers.forEach(answer => { if (answer === true) correctAnswers += 1 })
-    console.log(correctAnswers, saveGame.coins)
     if (correctAnswers !== saveGame.coins) return false
     return true
   }
 
   return (
-  <div className='home'>
-    <h1 className='home-h1'>MEGA QUIZZ</h1>
-    <NavLink to={`/quizz/${saveGame}`}
-    className='go-button-container'>
-      <div className='go-button'>
-        <span>Go!</span>
-      </div>
-    </NavLink>
-    <div className='prizewheel-container'>
-      <NavLink to='/prizewheel' className='prizewheel-button'>
-        <span>La Routourne</span>
-        <img src={WheelIcon} alt='wheel icon'/>
+    <div className='home'>
+      <h1 className='home-h1'>MEGA QUIZZ</h1>
+      <NavLink to={`/quizz/${saveGame}`} className='go-button-container'>
+        <div className='go-button'>
+          <span>Go!</span>
+        </div>
       </NavLink>
+      <div className='prizewheel-container'>
+        <NavLink to='/prizewheel' className='prizewheel-button'>
+          <span>La Routourne</span>
+          <img src={WheelIcon} alt='wheel icon'/>
+        </NavLink>
+      </div>
+      <form className="resume-game" onSubmit={handleSubmit}>
+        <input className='saveKey-input' placeholder='Code de sauvegarde ?' type='text' value={input} onInput={e => setInput(e.target.value)}></input>
+        <span className='input-error'>{errorMessage}</span>
+      </form>
     </div>
-    <form className="resume-game" onSubmit={handleSubmit}>
-
-      <input className='saveKey-input' placeholder='Code de sauvegarde ?' type='text' value={input} onInput={e => setInput(e.target.value)}></input>
-      <span className='input-error'>{errorMessage}</span>
-    </form>
-  </div>
   )
 }
 
